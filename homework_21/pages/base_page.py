5fce14050694718eb68bcc1737885d97d663fc22
MarikoShiden <marikoshiden@gmail.com>
2023-11-15 22:09:41 +0100

    Homework 23, page object

---
+++ homework_21/pages/base_page.py
@@ -0,0 +1,15 @@
+from abc import ABC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+
+class BasePage(ABC):
+    def __init__(self, driver):
+        self._driver = driver
+        self.web_driver_wait = WebDriverWait(self._driver, 10)
+
+    def wait_until_element_appears(self, locator):
+        return self.web_driver_wait.until(ec.presence_of_element_located(locator))
+
+    def _click(self, locator):
+        return self.wait_until_element_appears(locator).click()
